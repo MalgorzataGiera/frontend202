bf8e23d766c587c64b9d540f6a942a51e0140560
MalgorzataGiera <gosgie02@wp.pl>
2025-01-02 12:36:54 +0100

    display products

--- frontend-lab-app/app/page.js
+++ frontend-lab-app/app/page.js
@@ -1,30 +1,59 @@
-'use client'
+"use client";
 import Image from "next/image";
-import { useAuth } from '@/app/_lib/AuthContext';
-import { useRouter } from 'next/navigation';
+import { useAuth } from "@/app/_lib/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/app/_lib/firebase";
+import "./products.css";
 
-
+// const products = [
+//   { id: 1, name: "Product 1", price: "$10", image: "/images/product1.jpg" },
+//   { id: 2, name: "Product 2", price: "$20", image: "/images/product2.jpg" },
+//   { id: 3, name: "Product 3", price: "$30", image: "/images/product3.jpg" },
+// ];
 
 export default function Home() {
-  const { user } = useAuth();
-  const router = useRouter();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  // Jeśli użytkownik nie jest zalogowany, przekieruj go na stronę logowania
-  if (!user) {
-    // router.push('/login');
-  }
+  // const { user } = useAuth();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "products"));
+        const fetchedProducts = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setProducts(fetchedProducts);
+      } catch (error) {
+        console.error("Error fetching products: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        /> </main>        
+    <div className="container">
+      <h2>Available Products</h2>
+      <div className="grid">
+        {products.map((product) => (
+          <div key={product.productId} className="card">
+            <img src={product.ImgLink} alt={product.Name} className="image" />
+            <h3>{product.Name}</h3>
+            <p>
+              <strong>${product.Price}</strong>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
